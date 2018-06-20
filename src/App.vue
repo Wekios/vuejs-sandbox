4801279ea4e004c560b3dba65461fbf390570518
Wekios <diwanee@Bojans-MacBook-Pro.local>
2018-06-20 11:01:11 +0200

    Wonderful Quotes Project - Finished

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,49 @@
 <template>
-    <div class="container">
-    
-    </div>
+	<div class="container">
+		<app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes" ></app-header>
+		<app-new-quote @quoteAdded="newQuote"></app-new-quote>	
+		<appQuoteGrid :quotes="quotes" @quoteDeleted="deleteQuote"></appQuoteGrid>
+		<div class="row">
+			<div class="col-sm-12 text-center">
+				<div class="alert alert-info">
+					Info: Click On a Quote to delete it
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-    export default {
-        
+import Header from "./components/Header.vue";
+import QuoteGrid from "./components/QuoteGrid.vue";
+import NewQuote from "./components/NewQuote.vue";
+
+export default {
+  data() {
+    return {
+      quotes: ["Just a quote to see something"],
+      maxQuotes: 10
+    };
+  },
+  methods: {
+    newQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert("Maximum Quote Capacity Reached");
+      }
+      this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
     }
+  },
+  components: {
+    appHeader: Header,
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote
+  }
+};
 </script>
+
 
 <style>
 </style>
