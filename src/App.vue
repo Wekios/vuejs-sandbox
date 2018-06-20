27c68509e2a31a752e28e601809c73358e8a0a82
Wekios <diwanee@Bojans-MacBook-Pro.local>
2018-06-20 17:04:37 +0200

    Assignment 9 Forms - End

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,109 @@
 <template>
     <div class="container">
-        <form>
-            <div class="row">
+        <div v-if="!submited" class="row">
+            <form>
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <!-- Exercise 1 -->
-                    <!-- Create a Signup Form where you retrieve the following Information -->
-                    <!-- Full Name (First Name + Last Name) -->
-                    <!-- Mail -->
-                    <!-- Password -->
-                    <!-- Store Data? Yes/No -->
+                    <h1>Sign Up Form</h1>
+                    <hr>
 
-                    <!-- Exercise 2 -->
-                    <!-- Only display the Form if it has NOT been submitted -->
-                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+                    <div class="form-group">
+                        <label for="firstname"> First Name</label>
+                        <input 
+                            type="text" 
+                            id="firstname" 
+                            class="form-control"
+                            v-model="userData.firstName">
+                    </div>
+                    
+                    <div class="form-group">
+                        <label for="lastname"> Last Name</label>
+                        <input 
+                            type="text" 
+                            id="lastname" 
+                            class="form-control"
+                            v-model="userData.lastName">
+                    </div>
 
-                    <!-- Exercise 3 -->
-                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
-                    <!-- which still holds the First Name and Last Name Input Field -->
+                    <div class="form-group">
+                        <label for="email">Mail</label>
+                        <input
+                                type="text"
+                                id="email"
+                                class="form-control"
+                                v-model="userData.email">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input
+                                type="password"
+                                id="password"
+                                class="form-control"
+                                v-model="userData.password">
+                    </div>
+                    <label for="storage">Store in Database?</label>
+                    <br>
+                    <label for="yes">
+                        <input
+                                type="radio"
+                                id="yes"
+                                value="Yes"
+                                v-model="storage"> Yes
+                    </label>
+                    <label for="no">
+                        <input
+                                type="radio"
+                                id="no"
+                                value="No"
+                                v-model="storage"> No
+                    </label>
                 </div>
-            </div>
-        </form>
-        <hr>
-        <div class="row">
+
+                    <br>
+                    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                        <button
+                            class="btn btn-primary"
+                            @click.prevent="submited=true">Submit!
+                        </button>
+                    </div>
+            </form>
+        </div>
+
+        <div v-else class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
+                        <p>Full Name: {{ userData.firstName }} {{ userData.lastName }} </p>
+                        <p>Mail: {{ userData.email }} </p>
+                        <p>Password: {{userData.password}} </p>
+                        <p>Store in Database? {{ storage }}. </p>
                     </div>
                 </div>
             </div>
         </div>
+
     </div>
 </template>
 
 <script>
-    export default {
-    }
+export default {
+  data() {
+    return {
+      userData: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: ""
+        // baseSwitch: true
+      },
+      storage: "Yes",
+      submited: false
+    };
+  }
+};
 </script>
 
 <style>
