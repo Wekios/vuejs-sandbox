56f37082bb5ea0d377e98a0c72d4ff4d9cf366b5
Wekios <diwanee@Bojans-MacBook-Pro.local>
2018-06-21 14:13:58 +0200

    Section 12 Custom Directive Course Code

--- src/App.vue
+++ src/App.vue
@@ -1,109 +1,59 @@
 <template>
     <div class="container">
-        <div v-if="!submited" class="row">
-            <form>
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <h1>Sign Up Form</h1>
-                    <hr>
-
-                    <div class="form-group">
-                        <label for="firstname"> First Name</label>
-                        <input 
-                            type="text" 
-                            id="firstname" 
-                            class="form-control"
-                            v-model="userData.firstName">
-                    </div>
-                    
-                    <div class="form-group">
-                        <label for="lastname"> Last Name</label>
-                        <input 
-                            type="text" 
-                            id="lastname" 
-                            class="form-control"
-                            v-model="userData.lastName">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="email">Mail</label>
-                        <input
-                                type="text"
-                                id="email"
-                                class="form-control"
-                                v-model="userData.email">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input
-                                type="password"
-                                id="password"
-                                class="form-control"
-                                v-model="userData.password">
-                    </div>
-                    <label for="storage">Store in Database?</label>
-                    <br>
-                    <label for="yes">
-                        <input
-                                type="radio"
-                                id="yes"
-                                value="Yes"
-                                v-model="storage"> Yes
-                    </label>
-                    <label for="no">
-                        <input
-                                type="radio"
-                                id="no"
-                                value="No"
-                                v-model="storage"> No
-                    </label>
-                </div>
-
-                    <br>
-                    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                        <button
-                            class="btn btn-primary"
-                            @click.prevent="submited=true">Submit!
-                        </button>
-                    </div>
-            </form>
-        </div>
-
-        <div v-else class="row">
+        <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4>Your Data</h4>
-                    </div>
-                    <div class="panel-body">
-                        <p>Full Name: {{ userData.firstName }} {{ userData.lastName }} </p>
-                        <p>Mail: {{ userData.email }} </p>
-                        <p>Password: {{userData.password}} </p>
-                        <p>Store in Database? {{ storage }}. </p>
-                    </div>
-                </div>
+                <h1>Built-in Directives</h1>
+                <p v-text="'Some Text'"></p>
+                <p v-html="'<strong>Some strong text</strong>'"></p>
             </div>
         </div>
-
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Custom Directives</h1>
+                <p v-highlight:background.delayed="'red'">Color this</p>
+                <p v-local-highlight:background.delayed.blink="{mainColor: 'red', secondColor: 'green', delay: 500}">Color this, too</p>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      userData: {
-        firstName: "",
-        lastName: "",
-        email: "",
-        password: ""
-        // baseSwitch: true
-      },
-      storage: "Yes",
-      submited: false
-    };
-  }
-};
+    export default {
+        directives: {
+            'local-highlight': {
+                bind(el, binding, vnode) {
+                    var delay = 0;
+                    if (binding.modifiers['delayed']) {
+                        delay = 3000;
+                    }
+                    if (binding.modifiers['blink']) {
+                        let mainColor = binding.value.mainColor;
+                        let secondColor = binding.value.secondColor;
+                        let currentColor = mainColor;
+                        setTimeout(() => {
+                            setInterval(() => {
+                                currentColor == secondColor ? currentColor = mainColor : currentColor = secondColor;
+                                if (binding.arg == 'background') {
+                                    el.style.backgroundColor = currentColor;
+                                } else {
+                                    el.style.color = currentColor;
+                                }
+                            }, binding.value.delay);
+                        }, delay);
+                    } else {
+                        setTimeout(() => {
+                            if (binding.arg == 'background') {
+                                el.style.backgroundColor = binding.value.mainColor;
+                            } else {
+                                el.style.color = binding.value.mainColor;
+                            }
+                        }, delay);
+                    }
+                }
+            }
+        }
+    }
 </script>
 
 <style>
