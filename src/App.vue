9c8de1f80ea9ec47ad22aaef0fc500b531ffcbb8
Wekios <diwanee@Bojans-MacBook-Pro.local>
2018-06-26 11:46:44 +0200

    Http requests with vue - Start

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,18 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Directives Exercise</h1>
-                <button v-customOn:click="clicked"
-                class="btn btn-primary">Click Me</button>
-                <hr>
-                <div style="width:100px;height:100px;background-color:lightgreen"
-                v-customOn:mouseenter="mouseEnter"
-                v-customOn:mouseleave="mouseLeave"></div>
+                <h1>Http</h1>
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input class="form-control" type="text" v-model="user.username" />
+                </div>
+                <div class="form-group">
+                    <label for="mail">Mail</label>
+                    <input class="form-control" type="text" v-model="user.email" />
+                </div>
+                <button class="btn btn-primary" @click="submit">
+                    Submit
+                </button>
             </div>
         </div>
     </div>
@@ -16,27 +21,17 @@
 
 <script>
 export default {
-  directives: {
-    customOn: {
-      bind(el, binding) {
-        // el.onclick = function() {
-        //   binding.value();
-        // };
-        const type = binding.arg;
-        const fn = binding.value;
-        el.addEventListener(type, fn);
+  data() {
+    return {
+      user: {
+        username: "",
+        email: ""
       }
-    }
+    };
   },
   methods: {
-    clicked() {
-      alert("I was clicked!");
-    },
-    mouseEnter() {
-      console.log("Mouse entered");
-    },
-    mouseLeave() {
-      console.log("Mouse left");
+    submit() {
+      console.log(this.user);
     }
   }
 };
